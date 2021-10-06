437dd1a8d418a5583644c1cfc5b9266cb86e597e
Jin Zihang <zihang.jin@gmail.com>
2021-10-06 17:39:15 +0800

    Initialize main

---
+++ CA1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main() {
+
+    return 0;
+}
