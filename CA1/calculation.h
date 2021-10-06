b9969597ec658f019f1ce60abf6609f830acf9f0
Bryant Juspi <bjuspi@outlook.com>
2021-10-06 22:36:36 +0800

    added history struct

--- CA1/calculation.h
+++ CA1/calculation.h
@@ -6,8 +6,21 @@
 #include "struct.h"
 #include "enum.h"
 
-void CalculateProperties(enum shape shape) {
-    
+void CalculateProperties(enum shape shape, struct History *history)
+{
+    int rectangleCount = 0;
+
+    switch (shape)
+    {
+    case Rectangle:
+        history->rectangles[rectangleCount].length = 2;
+        history->rectangles[rectangleCount].width = 3;
+        history->rectangles[rectangleCount].area = 6;
+        history->rectangles[rectangleCount].perimeter = 10;
+        break;
+    default:
+        break;
+    }
 }
 
 #endif
