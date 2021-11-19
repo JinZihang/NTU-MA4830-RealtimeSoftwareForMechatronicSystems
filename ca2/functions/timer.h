eb5a690f2b83883cf3f4c62df8f898cc976a7146
Bryant Juspi <bjuspi@outlook.com>
2021-11-19 10:29:37 +0800

    Added comments

--- ca2/functions/timer.h
+++ ca2/functions/timer.h
@@ -1,3 +1,9 @@
+//******************************************************************************
+// Timer Functions
+// TimerInitialization: Initialize a timer with default configuration values
+// UpdateTimer: Update timer countdown if there is a change in wave parameters
+//******************************************************************************
+
 #ifndef METRONOME_TIMER_H
 #define METRONOME_TIMER_H
 
