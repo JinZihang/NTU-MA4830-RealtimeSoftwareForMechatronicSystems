893aa668a474170e2fed84092f058789057e75b2
JinZihang <zihang.jin@gmail.com>
2021-11-22 10:38:46 +0800

    Add comments to main.h

--- ca2/main.h
+++ ca2/main.h
@@ -1,3 +1,11 @@
+//******************************************************************************
+// Signal Handler Functions and Main Function
+// Define global variables used across the program
+// termination_signal_handler: handle ctrl-c signal
+// timer_signal_handler: handle alarm signal
+// main: main structure of the program
+//******************************************************************************
+
 #ifndef METRONOME_MAIN_H
 #define METRONOME_MAIN_H
 
